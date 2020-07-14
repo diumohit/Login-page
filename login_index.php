b623a624b9b7f4c7a4f4cb6216b3e37e0752491d
antar <diuswemohit@gmail.com>
2020-07-14 12:32:20 +0600

    Login Option

---
+++ login_index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>login page</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+
+
+        <h1 >Login or Sign Up</h1>
+        <div class="login_section">
+            <br><br>
+
+            User Name:
+            <input type="text" name="username" placeholder="Enter Your username Here">
+            <br><br>
+            Password:  
+            <input type="password"  class="pass" name="password" placeholder="Enter Your password Here"><br>
+            <input type="submit" value="Login" class="log" name="login_btn">
+
+        </div>
+
+
+    </form>
+
+</body>
+
+</html>
