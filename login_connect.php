9fef1a675805740e60cd48df810ff354e2cdda14
antar <diuswemohit@gmail.com>
2021-01-30 21:54:11 +0600

    update 2021

--- login_connect.php
+++ login_connect.php
@@ -3,7 +3,7 @@
 $db_hostname = "localhost";
 $db_user = "root";
 $db_password = "";
-$db_name = "coderhousebd";
+$db_name = "phploginproject";
 
 $connect = mysqli_connect($db_hostname,$db_user,$db_password,$db_name);
 
